d6c28756d9e4037eb01b10e0756a5ce6d6880d95
Oneil Williams <oneilswilliams@gmail.com>
2019-08-16 19:32:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,10 @@ def monopoly_with_third_tier
 	      :four_pieces_owned => 200
 	    } 
 	    :names => {
-	      
+	      :reading_rainbow => {},
+	      :pennsylvania_rainbow => {},
+	      :b_and_o_railroad => {},
+	      :shortline_railroad => {}
 	    }
 	   }
 	}
